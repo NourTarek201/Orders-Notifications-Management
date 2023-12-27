0dcab71389d3d1e4c1a6c5a7481f43c4a8480e4a
nourelshaer <nour.elshaer2003@gmail.com>
2023-12-27 19:43:39 +0200

    project

---
+++ OrderNotificationModule/src/main/java/com/example/ordernotificationmodule/OrderNotificationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordernotificationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderNotificationModuleApplication.class, args);
+	}
+
+}
---
+++ OrderNotificationModule/src/test/java/com/example/ordernotificationmodule/OrderNotificationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordernotificationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderNotificationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
